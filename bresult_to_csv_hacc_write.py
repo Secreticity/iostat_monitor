ce865e7127ebf3cbe4eb9fa30cb3b74aa748dcec
Chungyong Kim <chungyongkim@gmail.com>
2020-03-29 02:19:32 +0900

    200329-0219

--- bresult_to_csv_hacc_write.py
+++ bresult_to_csv_hacc_write.py
@@ -4,11 +4,11 @@ import pandas as pd
 #---------------- Save Settings ------------------
 
 # File name to save DataFrame into csv
-save_name = "out_mod8p_hacc"
+save_name = "out_orgp_hacc"
 #save_name = "org_npb"
 
 # File name of the iostat/throughput result
-out_file = "out_mod8p"
+out_file = "out_orgp"
 #-------------------------------------------------
 
 state = "INIT"
