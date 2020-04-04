60b6b8e8e4e772fb848ae1ecbb7fd4826689a26f
Chungyong Kim <chungyongkim@gmail.com>
2020-04-05 02:53:56 +0900

    200405-0253

--- n_profiling.sh
+++ n_profiling.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #------------ SETTING VARIABLES
-filepath='/home/kau/jwbang/200320/out_mod4s1.txt'
+filepath='/home/kau/jwbang/200320/out_mod16s1.txt'
 
-annot="out_mod4s1" #1,2,4,8,16
+annot="out_mod16s1" #1,2,4,8,16
 #path="/home/kau/jwbang/linux-5.2.8_org/mymodule/mymodule.ko"
 
 path="/home/kau/jwbang/linux-5.2.8_final/mymodule/mymodule.ko"
--- profiling.sh
+++ profiling.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #------------ SETTING VARIABLES
-filepath='/home/kau/jwbang/200320/out_mod4s1.txt'
+filepath='/home/kau/jwbang/200320/out_mod16s1.txt'
 
-annot="out_mod4s1"
+annot="out_mod16s1"
 #path="/home/kau/jwbang/linux-5.2.8_org/mymodule/mymodule.ko"
 
 #annot="MODIFIED2"
